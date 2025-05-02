aeef3033ac90992c637dcfd8252b1f6891795935
Divine Patrick <132227658+divinepatrick@users.noreply.github.com>
2025-05-02 23:17:10 +0100

    Create triangle.js
    Looping a triangle (Module 1 Exercise)

---
+++ Module 1/triangle.js
@@ -0,0 +1,17 @@
+// this block of code creates a triangle using a string of 7 "#".
+
+for (let i = "#"; i.length <= 7; i+= "#") {
+  console.log(i);
+}
+
+/* Like this;
+
+#
+##
+###
+####
+#####
+######
+#######
+
+*/
