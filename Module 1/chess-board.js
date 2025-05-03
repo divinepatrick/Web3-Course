42082c39ee3345e8aae77f20092d97627c787751
Divine Patrick <132227658+divinepatrick@users.noreply.github.com>
2025-05-03 18:15:15 +0100

    Update chess-board.js

--- Module 1/chess-board.js
+++ Module 1/chess-board.js
@@ -1,14 +1,16 @@
-let size = 8;
+let size = prompt("How big is your chess", Number());
 let grid = "";
 
 for (let i = 0; i < size; i++) {
   for (let j = 0; j < size; j++) {
+    // Add space or # to grid based on (i + j) % 2
       if ((i + j) % 2 === 0) {
           grid += " ";
       } else {
           grid += "#";
-      } 
+      }
   }
+  // Add newline character to grid
     grid += "\n";
 }
 
